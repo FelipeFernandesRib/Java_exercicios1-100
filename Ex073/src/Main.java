a85019550b55209b7fe810b2a1a505998884fcf6
Felipe Fernandes Ribeiro <156134328+FelipeFernandesRib@users.noreply.github.com>
2024-12-24 19:39:17 -0300

    Ex073
     VETORES

---
+++ Ex073/src/Main.java
@@ -0,0 +1,20 @@
+/*
+73) Crie um programa que preencha automaticamente (usando lógica, não apenas
+atribuindo diretamente) um vetor numérico com 10 posições, conforme abaixo:
+9 8 7 6 5 4 3 2 1 0
+0 1 2 3 4 5 6 7 8 9
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int [] num = new int[10];
+
+        for(int i = 0; i < 10; i++){
+            num[i] = 9 - i;
+        }
+
+        for(int exibir : num){
+            System.out.print(exibir + " ");
+        }
+    }
+}
